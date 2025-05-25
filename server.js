1ac0203189b5387d9ec29b8f79a2df59785aa03b
Beyza Tercan <tercabey@studetns.zhaw.ch>
2025-05-25 17:47:11 +0200

    GitHub Actions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! I am Beyza. Now Docker Deployement...');
+  res.send('Hello FS2025 DevOps Course! I am Beyza. Now Docker Deployement with GitHub Actions!');
 });
 
 app.listen(PORT, HOST);
