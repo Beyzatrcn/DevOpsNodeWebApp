58f804c446f12b1c92f9b756af27702b1cd71ec9
Beyza Tercan <tercabey@studetns.zhaw.ch>
2025-05-25 14:13:35 +0200

    Render Test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Iam Beyza. Cloud Deploy auf Render.com');
+  res.send('Hello FS2025 DevOps Course! I am Beyza. Cloud Deploy auf Render.com');
 });
 
 app.listen(PORT, HOST);
